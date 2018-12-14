8f01bb5c421cc9f0703dbc4ad359c7225a8f3bb0
github username <github email address>
2018-12-14 17:11:11 -0500

    friday

--- app/serializers/deck_serializer.rb
+++ app/serializers/deck_serializer.rb
@@ -2,7 +2,6 @@ class DeckSerializer < ActiveModel::Serializer
   attributes :id, :name, :user, :cards
 
    def cards
-     
      object.cards.map do |card|
        CardSerializer.new(card, scope: scope, root: false)
      end
